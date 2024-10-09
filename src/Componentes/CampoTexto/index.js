593f2db3c3a7b5b96fb4b5cfe811c606ee4bfd92
Heitor <heitor.moreira.neto@outlook.com>
2024-10-09 14:54:34 -0300

    first commit

---
+++ src/Componentes/CampoTexto/index.js
@@ -0,0 +1,12 @@
+import './CampoTexto.css'
+
+const CampoTexto = () => {
+    return (
+        <div>
+            <p>testando</p>
+            <input placeholder='testando' />
+        </div>
+    )
+}
+
+export default CampoTexto
---
+++ src/Componentes/Formulario/index.js
@@ -0,0 +1,13 @@
+
+import CampoTexto from '../CampoTexto'
+import './Formulario.css'
+
+const Formulario = () => {
+    return (
+        <div>
+            <CampoTexto />
+        </div>
+    )
+}
+
+export default Formulario
